864531a16c6738793e12c95547acd4bec8314ced
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-27 23:57:14 +0300

    Skills section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage/HomePage";
 import Header from "./components/Header/Header";
 import MyProjects from "./components/MyProjects/MyProjects";
 import ProjectDetailsPage from "./components/ProjectDetailsPage/ProjectDetailsPage";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/my-projects" element={<MyProjects />} />
         <Route path="/my-projects/:project" element={<ProjectDetailsPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
