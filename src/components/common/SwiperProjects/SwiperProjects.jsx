cdc0222fb92b6a1b0d045110f87a63a66f0ca3d8
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-28 14:13:09 +0300

    Responsible design - fixed

--- src/components/common/SwiperProjects/SwiperProjects.jsx
+++ src/components/common/SwiperProjects/SwiperProjects.jsx
@@ -5,14 +5,30 @@ import "../../../../node_modules/swiper/swiper-bundle.min.css";
 import "swiper/css";
 import "./SwiperProjects.css";
 import repos from "../../../reposDB";
+import { useEffect, useState } from "react";
 
 const SwiperProjects = () => {
+  const [swipes, setSwipes ] = useState(2);
+  useEffect(() => {
+    if (window.innerWidth < 1240) {
+      setSwipes(1);
+    }
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth < 1240) {
+        setSwipes(1);
+      } else if (window.innerWidth > 1240) {
+        setSwipes(2);
+      }
+    })
+  }, [])
+
   return (
     <Swiper
       className="swiper"
       modules={[Navigation, Pagination]}
       spaceBetween={0}
-      slidesPerView={2}
+      slidesPerView={swipes}
       navigation
       pagination={{ clickable: true }}
     >
